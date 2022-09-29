dc581dd771d369d9820dd1ba53204d5854404cab
psalmon27 <psalmon@itba.edu.ar>
2022-09-29 12:19:56 -0300

    Punto 1 finiquitado

--- script.py
+++ script.py
@@ -1,8 +1,12 @@
-
 import numpy as np
 from Bio import SeqIO
 
 mRNA=SeqIO.read("NTRK1.gb","genbank")
-aa_seq=mRNA.translate()
+print(mRNA)
+
+'''
+aa_seq=mRNA.translate(to_stop=True)
+
 print(aa_seq)
 SeqIO.write(aa_seq,"aa_seq.fasta","fasta")
+'''

db5d9b1ff220ccc67c4b1167f0319693191e0026
psalmon27 <106761612+psalmon27@users.noreply.github.com>
2022-11-27 22:41:28 -0300

    config update

--- configuracion.py
+++ configuracion.py
@@ -2,12 +2,11 @@ import configparser
 configuracion = configparser.ConfigParser()
 
 configuracion['Búsqueda de ORFs'] = {'archivo' : 'NTRK1'}
-configuracion['Blast'] = {'archivo' : 'protein', 'Tipo' : 'blastp', 'BD' : 'nr', 'Resultados' : '100'}
+configuracion['Blast'] = {'archivo' : 'bestORF', 'Tipo' : 'blastp', 'BD' : 'nr', 'Resultados' : '100', 'Matriz' : 'BLOSUM62', 'E umbral' : '0.05'}
 configuracion['MSA'] = {'archivo' : 'secuencias'}
 configuracion['Búsqueda de pattern'] = {'archivo' : 'blast', 'pattern' : 'sapiens'}
-configuracion ['Dominios'] = {'archivo' : 'protein'}
+configuracion ['Dominios'] = {'archivo' : 'bestORF'}
 
 
 with open('entradas.cfg', 'w') as archivoconfig:
     configuracion.write(archivoconfig)
-

f6ec1b3983ab863ce2e189992bfa3896c84e0ef9
psalmon27 <106761612+psalmon27@users.noreply.github.com>
2022-11-28 23:32:00 -0300

    versión final

--- script.py
+++ script.py
@@ -159,7 +159,7 @@ bd=configuracion['Blast']['bd']
 resultados=int(configuracion['Blast']['resultados'])
 matriz=(configuracion['Blast']['matriz'])
 e_thres=float(configuracion['Blast']['E umbral'])
-´#callblast(query,tipo,bd,resultados,matriz,e_thres)
+#callblast(query,tipo,bd,resultados,matriz,e_thres)
 
 #3. MSA 
 
